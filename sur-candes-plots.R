9dbce097506206c73349f521a23247bb7cc02dcd
SebastianChk <86018240+SebastianChk@users.noreply.github.com>
2022-07-06 15:37:10 +0100

    Add files via upload

---
+++ sur-candes-plots.R
@@ -0,0 +1,243 @@
+setwd("C:/Users/sebas/Google Drive/Hamilton Internship 2022/code")
+# setwd("C:/Users/sebas/My Drive (chejnias@tcd.ie)/Hamilton Internship 2022/code")
+# Setup -------------------------------------------------------------------
+
+# plots folder/directory
+PD <- 'plots'
+
+# data folder/directory
+DD <- 'data'
+
+# experiment name (so that the resulting files can be differentiated by file name alone)
+exp.name <- ''
+
+library(fs)
+library(dplyr)
+library(purrr)
+library(ggplot2)
+# The Fig 4 and 5 plots take a very long time to run
+# setwd(...)
+set.seed(100)
+
+# set n and p
+n <- 4000
+p <- 800
+
+# sigmoid function for convenience
+sigmoid <- function(xb){return(exp(xb)/(1+exp(xb)))}
+
+# function that simulates covariate matrix - I created this function so that the
+# code can easily be generalised to an arbitrary distribution of the covariate
+# matrix by simply editing this function
+rcovariate <- function(n, p){
+  return(rnorm(n*p, 0, 1/sqrt(n)) %>% matrix(n, p))
+}
+
+# Fig 2 -------------------------------------------------------------------
+
+# create design matrix and parameter vector
+X <- rcovariate(n, p)
+beta <- c(rep(10, 1/8 * p),  rep(-10, 1/8 * p), rep(0, 3/4 * p))
+
+# Generate Y sample
+probs <- sigmoid(X %*% beta)
+Y <- rbinom(n, 1, probs)
+
+# Fit logistic regression to Y sample
+model1 <- glm(Y ~ X-1, family=binomial)
+
+# Prepare data for plotting
+df1 <- as.data.frame(beta)
+df1$index <- seq(1,p)
+df1$mle_beta <- model1$coefficients
+
+ggplot(df1) +
+  geom_point(aes(index, mle_beta), color='blue', size=1) +
+  geom_point(aes(index, beta), size=0.5) +
+  xlab('Index') +
+  ylab('Coefficients(true & fitted)') +
+  theme(aspect.ratio = 4/9)
+
+# Create 'plots' folder if it doesn't already exist
+file.path(getwd(), PD) %>%
+  dir.create()
+
+# Save figure
+ggsave(path_join(c(PD, paste(exp.name, 'fig2.png', sep=''))), dpi=900)
+
+
+# Fig 3 -------------------------------------------------------------------
+set.seed(100)
+# replace previous beta with N(3, 16) beta
+beta <- rnorm(p, 3, 4)
+
+# Sample Y and fit model
+probs <- sigmoid(X %*% beta)
+Y <- rbinom(n, 1, probs)
+model2 <- glm(Y ~ X-1, family=binomial)
+
+# put together data for fig 2A
+df2A <- as.data.frame(beta)
+df2A$mle_beta <- model2$coefficients
+
+# Fig 3A ------------------------------------------------------------------
+
+ggplot(df2A) +
+  geom_point(aes(beta, mle_beta), color='blue', size=1) +
+  geom_abline(intercept=0, slope=1, color='black', size=1) + 
+  geom_abline(intercept=0, slope=1.499, color='red', size=1) + 
+  xlab('True signal') +
+  ylab('MLE estimate') +
+  theme(aspect.ratio = 8/8)
+
+
+ggsave(path_join(c(PD, paste(exp.name, 'fig3A.png', sep=''))), dpi=500, height=5, width=5)
+
+# Fig 3B ------------------------------------------------------------------
+pred_probs <- predict(model2, as.data.frame(X), type='response')
+Xb <- X %*% beta
+
+df2B <- as.data.frame(cbind(Xb, probs, pred_probs))
+colnames(df2B) <- c('Xbeta', 'true_probs', 'pred_probs')
+df2B %>% head
+df2B <- df2B[order(df2B$Xbeta),]
+df2B %>% head
+df2B$order <- seq(1,n)
+
+# ggplot(df2B) +
+#   geom_point(aes(Xbeta, pred_probs), color='blue', size=0.8, alpha=1) +
+#   geom_line(aes(Xbeta, true_probs), size=1.4) +
+#   theme(aspect.ratio = 8/8) +
+#   ylab('Probabilities (True and predicted)')#+
+#   # scale_x_sqrt() #+ 
+#   # 
+# 
+# ggsave(path_join(c(PD, paste(exp.name, 'fig3B.png', sep=''))), dpi=500, height=5, width=5)
+
+ggplot(df2B) +
+  geom_point(aes(order, pred_probs), color='blue', size=0.9, alpha=0.4) +
+  geom_line(aes(order, true_probs), size=1.4) +
+  theme(aspect.ratio = 8/8) +
+  ylab('Probabilities (True and predicted)') + 
+  xlab('Observation index')
+
+ggsave(path_join(c(PD, paste(exp.name, 'fig3Bscaled.png', sep=''))),  dpi=500, height=5, width=5)
+
+
+# Fig 4 -------------------------------------------------------------------
+
+### NOTE: the p-value is actually 1 - p-value as I have defined it here
+
+
+set.seed(100)
+# keep beta fixed, with half zero and the others ~ N(7, 1)
+beta <- c(rep(0,p/2), rnorm(n=p/2, mean=7, sd=1)) %>% matrix(nrow=p, ncol=1)
+
+# number of monte carlo samples
+m <- 1000
+
+# placeholder matrices which will be replaced with the relevant samples
+# more efficient than appending on each iteration I think
+nullcoeffs <- matrix(rep(-1,p/2), nrow=p/2, ncol=m)
+SEbeta1 <- rep(-1, m)
+
+LLs <- matrix(rep(-1,2*m), nrow=m)
+
+start <- Sys.time()
+for(j in 1:m){
+
+  # sample X and Y
+  Xj <- rcovariate(n, p)
+  probsj <- sigmoid(Xj %*% beta)
+  Yj <- rbinom(n, 1, probsj) %>% matrix(n, 1)
+  
+  # fit full model
+  modelj <- glm(Yj ~ Xj-1, family=binomial)
+  
+  # fit null model
+  Xjnull <- Xj[,2:p]
+  modeljnull <- glm(Yj ~ Xjnull-1, family=binomial)
+  
+  # Extract log-likelihoods
+  LLj <- modelj$deviance
+  LLjnull <- modeljnull$deviance
+  # LLR <- LLjnull - LLj
+  # pval <- pchisq(LLR, df=1)
+  LLs[j,] <- c(LLj, LLjnull)
+  
+  # update matrix with null coefficient estimates
+  nullcoeffs[,j] <- modelj$coefficients[1:round(p/2)]
+  
+  # update matrix with the estimate of beta0's SE
+  SEbeta1[j] <- summary(modelj)$coefficients[, 2][1]
+  
+  # keep track of progress
+  print(j)
+}
+pbeta1 <- 1 - pchisq(LLs[,2]-LLs[,1], df=1)
+pbeta1 %>% hist
+
+
+# create /data folder if it doesn't already exist
+file.path(getwd(), "data") %>%
+  dir.create()
+
+# Save results of the above for loop (it takes a while to run)
+save(nullcoeffs, file=path_join(c(DD, paste(exp.name, 'nullcoeffs-fig4.RData', sep=''))))
+save(SEbeta1, file=path_join(c(DD, paste(exp.name, 'SEbeta1-fig4.RData', sep=''))))
+save(pbeta1, file=path_join(c(DD, paste(exp.name, 'pbeta1-fig5.RData', sep=''))))
+
+load(path_join(c(DD, paste(exp.name, 'nullcoeffs-fig4.RData', sep=''))))
+load(path_join(c(DD, paste(exp.name, 'SEbeta1-fig4.RData', sep=''))))
+load(path_join(c(DD, paste(exp.name, 'pbeta1-fig5.RData', sep=''))))
+
+# Fig 4A ------------------------------------------------------------------
+set.seed(100)
+# Re-load the beta vector if necessary
+# beta <- c(rep(0,400), rnorm(400, 7, 1))
+
+mc_ses <- apply(nullcoeffs, 1, sd)
+ggplot() + 
+  geom_histogram(aes(mc_ses), color='black', fill='#990000', bins=50) +
+  geom_vline(xintercept=2.663, linetype='dashed') +
+  theme(aspect.ratio = 8/8) +
+  ylab('Relative Counts') +
+  xlab('SEs of coefficients')
+  
+colors <- c("y=1.499x, using modern big p theory"="")
+ggsave(path_join(c(PD, paste(exp.name, 'fig4A.png', sep=''))), dpi=900, height=5, width=5)
+
+
+# Fig 4B ------------------------------------------------------------------
+
+ggplot() + 
+  geom_histogram(aes(SEbeta1), color='black', fill='#6699FF') +
+  theme(aspect.ratio = 8/8) +
+  ylab('Relative Counts') +
+  xlab('SEs of coefficients')
+
+ggsave(path_join(c(PD, paste(exp.name, 'fig4B.png', sep=''))), dpi=900, height=5, width=5)
+
+
+# Fig 5 -------------------------------------------------------------------
+
+pbeta1 %>% hist
+
+ggplot() +
+  geom_histogram(aes(pbeta1),
+                 color='red',
+                 fill='#000099',
+                 boundary=0, bins=40) +
+  theme(aspect.ratio=9/18) +
+  xlim(0,1) +
+  # coord_cartesian(xlim=c(0,1)) +
+  ylab('Relative Counts') +
+  xlab('P-Values')
+
+ggsave(path_join(c(PD, paste(exp.name, 'fig5temp.png', sep=''))), dpi=900, height=5, width=10)
+
+pbeta1
+  
+
+
+
---
+++ zollanvari-genton-plots.R
@@ -0,0 +1,463 @@
+# setwd("C:/Users/sebas/Google Drive/Hamilton Internship 2022/code")
+# setwd("C:/Users/sebas/My Drive (chejnias@tcd.ie)/Hamilton Internship 2022/code")
+
+library(dplyr)
+library(ggplot2)
+library(MASS)
+library(data.table)
+library(reshape2)
+library(tidyverse)
+library(stringr)
+library(fs)
+
+PD <- 'plots_lda'
+
+file.path(getwd(), PD) %>%
+  dir.create()
+
+# Error estimators --------------------------------------------------------
+
+
+# First I need functions to evaluate explicitly all the errors.
+
+# Anderson's W statistic
+W.statistic <- function(Sigma.inv, Xbar0, Xbar1, Xi){
+  # This function assumes that Sigma is a diagonal matrix
+  a <- (Xi - (Xbar0 + Xbar1)/2)
+  b <- diag(Sigma.inv)
+  c <- (Xbar0 - Xbar1)
+  return(sum(a*b*c))
+}
+
+# Resubstitution error estimator
+resub <- function(Sigma.inv, Xbar0, Xbar1, X, n, n0=0, n1=0){
+  if(n0==0){
+    n0 <- n/2
+    n1 <- n/2
+  }
+  W.func <- function(X){return(W.statistic(Sigma.inv, Xbar0, Xbar1, X))}
+  W <- apply(X, 1, W.func)
+  
+  W0 <- W[1:n0]
+  e0 <- mean( W0 <= 0)
+  a0 <- n0/n
+  
+  W1 <- W[round(n0+1):n]
+  e1 <- mean( W1 >  0)
+  a1 <- n1/n
+  return(a0*e0 + a1*e1)
+}
+
+# Plug-in error estimator
+plugin <- function(Sigma.inv, Xbar0, Xbar1, X){
+  a <- (Xbar0 - Xbar1)^2
+  b <- diag(Sigma.inv)
+  deltahat <- sqrt( sum(a*b) )
+  return(pnorm(-deltahat/2))
+}
+
+# Smoothed resubstitution error estimator
+smoothed.resub <- function(Sigma.inv, Xbar0, Xbar1, X, b, deltahat, n, n0=0, n1=0){
+  if(n0==0){
+    n0 <- n/2
+    n1 <- n/2
+  }
+  g <- function(x){pnorm(-x/(b*deltahat))}
+  
+  W.func <- function(X){return(W.statistic(Sigma.inv, Xbar0, Xbar1, X))}
+  W <- apply(X, 1, W.func)
+
+  W0 <- W[1:n0]
+  e0 <- mean(   g(W0) )
+  a0 <- n0/n
+  
+  W1 <- W[round(n0+1):n]
+  e1 <- mean( 1-g(W1) )
+  a1 <- n1/n
+
+  return(a0*e0 + a1*e1)
+}
+
+# True error
+true.error <- function(Sigma.inv, Xbar0, Xbar1, mu0, mu1, n, n0=0, n1=0){
+  if(n0==0){
+    n0 <- n/2
+    n1 <- n/2
+  }
+  a0 <- n0/n
+  a1 <- n1/n
+
+  numerator0 <- sum( 
+    ( mu0-(Xbar0+Xbar1)/2 ) * diag(Sigma.inv) * (Xbar0-Xbar1) 
+    )
+  
+  numerator1 <- sum(
+    ( mu1-(Xbar0+Xbar1)/2 ) * diag(Sigma.inv) * (Xbar0-Xbar1) 
+    )
+  
+  denominator <- sqrt(sum(
+    (Xbar0-Xbar1)^2 * diag(Sigma.inv)
+    ))
+
+  e0 <- pnorm(-numerator0/denominator)
+  e1 <- pnorm(numerator1/denominator)
+
+  return(a0*e0 + a1*e1)
+}
+
+# Asymptotic-finite error expectations ------------------------------------
+# Note:
+
+# The limit of the expectation of the resub and plug-in estimators are the same.
+# Hence I only define a function for the resubsitution error expectation
+
+resub.af <- function(n, p, n0=0, n1=0, delta.squared=4){
+  if(n0==0){
+    n0 <- n/2
+    n1 <- n/2
+  }
+  J0 <- p/n0
+  J1 <- p/n1
+  
+  return(pnorm(-sqrt(delta.squared + J0 + J1)/2))
+}
+
+smoothed.resub.af <- function(n, p, b, n0=0, n1=0, delta.squared=4){
+  if(n0==0){
+    n0 <- n/2
+    n1 <- n/2
+  }
+  J0 <- p/n0
+  J1 <- p/n1
+  
+  return(pnorm( -sqrt(delta.squared + J0 + J1) / (2*sqrt(1+b^2)) ))
+}
+
+true.error.af <- function(n, p, n0=0, n1=0, delta.squared=4){
+  if(n0==0){
+    n0 <- n/2
+    n1 <- n/2
+  }
+  J0 <- p/n0
+  J1 <- p/n1
+  
+  a0 <- n0/n
+  a1 <- n1/n
+  
+  denom <- sqrt(delta.squared + J0 + J1)
+  
+  e0 <- pnorm(-0.5*(delta.squared+J1-J0)/denom)
+  e1 <- pnorm(-0.5*(delta.squared-J1+J0)/denom)
+
+  return(a0*e0 + a1*e1)
+}
+
+
+
+
+# Redundant-finite error expectations -------------------------------------
+f <- function(a, h, n0, n1, J0, J1, a0, a1){
+  return(a0*pnorm(-0.5*a/sqrt(h(n0, n1, J0, J1))) + a1*pnorm(-0.5*a/sqrt(h(n1, n0, J1, J0))))
+  }
+
+true.error.rf <- function(n0, n1, J0, J1, a0, a1, delta.squared=4){
+  h <- function(n0, n1, J0, J1){
+    return(delta.squared * (1+1/n1)+ J0+ J1+ J1/(2*n1) + J0/(2*n0) )
+  }
+  a <- delta.squared+J1-J0
+  return(f(a, h, n0, n1, J0, J1, a0, a1))
+}
+
+# The paper makes the distinction between alpha and alpha hat, but in these
+# simulations we take these to be the same
+resub.rf <- function(n0, n1, J0, J1, a0, a1, delta.squared=4){
+  h <- function(n0, n1, J0, J1){
+    return(delta.squared * (1+1/n1)+ J0+ J1+ J1/(2*n1) + J0/(2*n0) )
+  }
+  a <- delta.squared+J1+J0
+  return(f(a, h, n0, n1, J0, J1, a0, a1))
+}
+
+plugin.rf <- function(n0, n1, J0, J1, a0, a1, delta.squared=4){
+  h <- function(n0, n1, J0, J1){
+    return(delta.squared * (1+1/n1+1/n0)+ J0+ J1+ J1/(2*n1) + J0/(2*n0) + J0/n1)
+  }
+  a <- delta.squared+J1+J0
+  return(f(a, h, n0, n1, J0, J1, a0, a1))
+}
+
+smoothed.resub.rf <- function(n0, n1, J0, J1, a0, a1, b, delta.squared=4){
+  h <- function(n0, n1, J0, J1){
+    return(delta.squared * (1+ b^2 +1/n1+1/(2*n0)) + J0/(2*n1)+ J1/(2*n1) + (1+b^2) * (J0 + J1))
+  }
+  a <- delta.squared+J1+J0
+  return(f(a, h, n0, n1, J0, J1, a0, a1))
+}
+
+# Monte Carlo Simulations -------------------------------------------------
+
+# Function to carry out one MC simulation
+lda.mc <- function(N, p, n, seed=99, n0=n/2, n1=n/2, delta.squared=4){
+  start <- Sys.time()
+  # For comments, see the previous section, 'Test simulation'
+  J0 <- p/n0
+  J1 <- p/n1
+  
+  set.seed(seed)
+  mu0 <- rnorm(p, mean=1, sd=2)
+  mu1 <- rep(0, p)
+  Sigma <- diag( p/4 *mu0^2, nrow=p, ncol=p)
+  Sigma.inv <- diag(1/diag(Sigma))
+  
+  # This mean and covariance works too, I just used the less trivial one that
+  # follows because I was experimenting and making sure that it did not affect
+  # the results. Both of these setups satisfy the key property: that the
+  # mahalanobis distance between the distribution means is 2
+  
+  # mu0 <- c(1, rep(0,p-1))
+  # mu1 <- c(0, rep(0,p-1))
+  # Sigma <- diag(c(1/delta.squared, rep(1,p-1)), nrow=p, ncol=p)
+  # Sigma.inv <- diag(1/diag(Sigma))
+  
+  errors <- matrix(rep(-10, 4*N), nrow=N, ncol=4)
+  colnames(errors) <- c('resub', 'plugin', 'sresub', 'true')
+  for(i in 1:N){
+    X <- rbind(mvrnorm(n0, mu0, Sigma), mvrnorm(n1, mu1, Sigma))
+    
+    Xbar0 <- apply(X[1:n0,], 2, mean)
+    Xbar1 <- apply(X[round(n0+1):n,], 2, mean)
+    
+    deltahat <- ((Xbar0 - Xbar1)^2 * diag(Sigma.inv)) %>% sum %>% sqrt
+    
+    # Relies on n_0=n_1
+    bopt <- 1/delta.squared * sqrt( (J1 + J0)*(J0 + J1 + 2*delta.squared) )
+    
+    errors[i, 'resub'] <- resub(Sigma.inv, Xbar0, Xbar1, X, n)
+    errors[i, 'plugin'] <- plugin(Sigma.inv, Xbar0, Xbar1, X)
+    errors[i, 'sresub'] <- smoothed.resub(Sigma.inv, Xbar0, Xbar1, X, bopt, deltahat, n)
+    errors[i, 'true'] <- true.error(Sigma.inv, Xbar0, Xbar1, mu0, mu1, n)
+  }
+  end <- Sys.time()
+  print(end-start)
+  return(errors)
+}
+
+# Asymptotic-finite sample approximation ----------------------------------
+lda.af <- function(p, n, n0=0, n1=0, delta.squared=4){
+  if(n0==0){
+    n0 <- n/2
+    n1 <- n/2
+  }
+  J0 <- p/n0
+  J1 <- p/n1
+  
+  bopt <- 1/delta.squared * sqrt( (J1 + J0)*(J0 + J1 + 2*delta.squared) )
+  
+  errors <- c()
+  c('resub', 'plugin', 'sresub', 'true')
+  errors['resub'] <- resub.af(n, p)
+  errors['plugin'] <- resub.af(n, p)
+  errors['sresub'] <- smoothed.resub.af(n, p, bopt)
+  errors['true'] <- true.error.af(n, p)
+  
+  return(errors)
+}
+
+
+# Redundant-finite sample approximation -----------------------------------
+lda.rf <- function(p, n, n0=0, n1=0, delta.squared=4){
+  if(n0==0){
+    n0 <- n/2
+    n1 <- n/2
+  }
+  J0 <- p/n0
+  J1 <- p/n1
+  
+  bopt <- 1/delta.squared * sqrt( (J1 + J0)*(J0 + J1 + 2*delta.squared) )
+  
+  errors <- c()
+  c('resub', 'plugin', 'sresub', 'true')
+  errors['resub'] <- resub.rf(n0, n1, J0, J1, n0/n, n1/n, delta.squared)
+  errors['plugin'] <- resub.rf(n0, n1, J0, J1, n0/n, n1/n, delta.squared)
+  errors['sresub'] <- smoothed.resub.rf(n0, n1, J0, J1, n0/n, n1/n, bopt, delta.squared)
+  errors['true'] <- true.error.rf(n0, n1, J0, J1, n0/n, n1/n, delta.squared)
+  
+  return(errors)
+}
+
+# Running everything for all n and p --------------------------------------
+
+# If this were python I would use a multi-index pandas dataframe in order to
+# plot this. My solution instead here is to basically create nested lists.
+
+# n and p values to find errors for
+ns <- c(40, 60, 80, 100, 120, 140, 160, 180, 200)
+ps <- c(3, 9, 30, 100)
+
+# estimators to find errors for
+est_names <- c('resub', 'plugin', 'sresub', 'true')
+# est_names <- c('plugin')
+
+
+# approximations to use
+approx_names <- c('mc', 'af')#, 'rf')
+
+# Number of Monte-Carlo samples
+N <- 1000
+
+# df is a dataframe of which copies will be made. results is a list that  will
+# have such a dataframe at its lowest level, and the dataframe will correspond
+# to the error estimates for a given estimator and approximation. The
+# rows/columns of the dataframe correspond to sample size/dimensionality
+# respectively
+results <- list()
+df <- data.frame(matrix(data=-1,
+                        ncol=length(ps),
+                        nrow=length(ns)),
+                 row.names=ns)
+colnames(df) <- ps
+
+for(i in approx_names){
+  est_list <- list()
+  for(j in est_names){
+    est_list[[j]] <- copy(df)
+  }
+  results[[i]] <- est_list
+}
+
+# Insert Monte-Carlo Simulation errors into results$mc
+
+for(n in ns){
+  for(p in ps){
+    mc.errors <- lda.mc(N, p, n)
+    mc.errors <- apply(mc.errors, 2, mean)
+    n.str <- as.character(n)
+    p.str <- as.character(p)
+    for(est_name in est_names){
+      # only doing the rounding temporarily
+      results$mc[[est_name]][n.str, p.str] <- mc.errors[est_name] 
+    }
+  }
+}
+
+# Insert asymptotic-finite sample approximation errors into results$af
+for(n in ns){
+  for(p in ps){
+    af.errors <- lda.af(p, n)
+    n.str <- as.character(n)
+    p.str <- as.character(p)
+    for(est_name in est_names){
+      # only doing the rounding temporarily
+      results$af[[est_name]][n.str, p.str] <- af.errors[est_name]
+    }
+  }
+}
+
+# redundant-finite (didn't include in report)
+# for(n in ns){
+#   for(p in ps){
+#     rf.errors <- lda.rf(p, n)
+#     n.str <- as.character(n)
+#     p.str <- as.character(p)
+#     for(est_name in est_names){
+#       # only doing the rounding temporarily
+#       results$rf[[est_name]][n.str, p.str] <- rf.errors[est_name]
+#     }
+#   }
+# }
+
+
+# Plots -------------------------------------------------------------------
+
+
+# Need to create an array/df for each individual plot. So, for fixed
+# est_name(plot), I define 8 curves, each corresponding to an element of the
+# cartesian product of {3,9,30,100} and {'mc', 'af'}
+
+# In hindsight, I should have really just made the original 'results' list to
+# have this structure at the lowest level. But at this point this is unnecessary
+# to fix since my code works
+df.cnames <- c('n', '3mc', '9mc', '30mc', '100mc', '3af', '9af', '30af', '100af')
+
+# actually not entirely sure if this works for the redundant-finite sample approximation, will have to re-check
+
+for(est_name in est_names){
+  # Need to create an array/df for each individual plot. So, for fixed
+  # est_name(plot), I define 8 curves, each corresponding to an element of the
+  # cartesian product of {3,9,30,100} and {'mc', 'af', 'rf'}
+  
+  # In hindsight, I should have really just made the original 'results' list to
+  # have this structure at the lowest level. But at this point this is
+  # unnecessary to fix since my code works
+  df <- data.frame(matrix(nrow=9, ncol=9))
+  
+  colnames(df) <- df.cnames
+  df$n <- ns
+  
+  # extract the corresponding results from 'results' list
+  for(ni in 1:length(ns)){
+    for(p in ps){
+      for(app in approx_names){
+        n.str <- as.character(ns[ni])
+        p.str <- as.character(p)
+        df[ni, paste(p, app, sep='')] <- results[[app]][[est_name]][n.str, p.str] 
+      }
+    }
+  }
+  
+  # Prepare dataframe for plotting
+  df.l <- melt(df, id.var='n')
+  df.l$approx <- df.l$variable %>% str_sub(-2, -1)
+  df.l$p <- df.l$variable %>% str_sub(end=-3) %>% factor(levels=c('3', '9', '30', '100'))
+  
+  convert <- function(string){
+    if(string=='af'){
+      return('Asymptotic approximation')
+    } else if(string=='mc'){
+      return('Monte Carlo simulation')
+    } else if(string=='rf'){
+      return('Redundant-finite approximation')
+    }
+  }
+  
+  df.l$approx <- apply(matrix(df.l$approx), 1, FUN=convert)
+  
+  # Create plot
+  plot.lda <- ggplot(data=df.l, aes(x=n, y=value, group=variable)) + 
+    geom_point(aes(colour=approx, shape=approx), size=2) +
+    scale_shape(solid = FALSE) +
+    geom_line(aes(colour=approx, linetype=p)) + 
+    labs(colour='Estimation method', shape='Estimation method') +
+    theme(aspect.ratio = 4/8)
+  
+  # Set y limits to (approximately) match those from the paper
+  ylims <- if (est_name=='true' || est_name=='sresub') c(0.15, 0.3) else c(0.025, 0.16)
+  plot.lda <- plot.lda + coord_cartesian(ylim=ylims) 
+  
+  # Bayes Error line
+  plot.lda <- plot.lda + geom_hline(yintercept=0.1586)
+  
+  # Remove y-label, cannot typeset greek letters in R
+  plot.lda <- plot.lda + ylab(NULL)
+  plot.lda
+  # Save plots
+  ggsave(path_join(c(PD, paste0(est_name,'.png'))), plot.lda, dpi = 500, height=4.5, width=10)
+}
+
+# save(errors, file='data/lda-errors.RData')
+# load('data/lda-errors.RData')
+# df.l
+# 
+# df.l$approx <- df.l$variable %>% str_sub(-2, -1)
+# 
+# convert <- function(string){
+#   if(string=='af'){
+#     return('Asymptotic-finite approximation')
+#   } else if(string=='mc'){
+#     return('Monte Carlo simulation')
+#   } else if(string=='rf'){
+#     return('Redundant-finite approximation')
+#   }
+# }
+# df.l$approx <- apply(matrix(df.l$approx), 1, FUN=convert)
+# df.l
